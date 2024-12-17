9419db675f6c04b434409a3b05a5841356310bb7
jeevan thapa <jeevanthapa@microsoft.com>
2024-12-17 10:48:23 -0500

    Register dependency injection

--- Api/src/UrlShortener.Api/Program.cs
+++ Api/src/UrlShortener.Api/Program.cs
@@ -1,14 +1,25 @@
 using Api.Extensions;
+using Azure.Identity;
 using UrlShortener.Core.Urls.Add;
+using UrlShortener.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultName = builder.Configuration["KeyVaultName"];
+if (!string.IsNullOrEmpty(keyVaultName))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{keyVaultName}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(TimeProvider.System);
-builder.Services.AddUrlFeature();
+builder.Services.AddUrlFeature()
+    .AddCosmosUrlDataStore(builder.Configuration);
 
 var app = builder.Build();
 
