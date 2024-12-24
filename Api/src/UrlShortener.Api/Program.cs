c92ac5e2bd3134ac3435304116ce2fa5636a5cbf
jeevan thapa <jeevanthapa@microsoft.com>
2024-12-24 13:00:10 -0500

    Add Authuentication and Authorization

--- Api/src/UrlShortener.Api/Program.cs
+++ Api/src/UrlShortener.Api/Program.cs
@@ -1,6 +1,12 @@
+using System.Security.Authentication;
+using System.Security.Claims;
 using Api;
 using Api.Extensions;
 using Azure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Polly;
 using UrlShortener.Core.Urls.Add;
 using UrlShortener.Infrastructure.Extensions;
 
@@ -32,6 +38,34 @@ builder.Services.AddHttpClient("TokenRangeService",
 builder.Services.AddSingleton<ITokenRangeApiClient, TokenRangeApiClient>();
 builder.Services.AddHostedService<TokenManager>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.TokenValidationParameters.NameClaimType = "name";
+    },
+    options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("AuthZPolicy", policyBuilder =>
+        policyBuilder.Requirements.Add(new ScopeAuthorizationRequirement()
+        {
+            RequiredScopesConfigurationKey = "AzureAd:Scopes"
+        }));
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder(
+            JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .Build();
+    // default, all incoming requests will be authorized the default policy
+    options.FallbackPolicy = options.FallbackPolicy;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -42,17 +76,24 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", () => "API");
+app.MapGet("/", () => "API")
+    .AllowAnonymous();
 
 app.MapPost("/api/urls",
     async (AddUrlHandler handler,
         AddUrlRequest request,
+        HttpContext context,
         CancellationToken cancellationToken) =>
     {
+        var email = context.User.FindFirstValue("preferred_username")
+            ?? throw new AuthenticationException("Missing preferred_username claim");
+        
         var requestWithUser = request with
         {
-            CreatedBy = "test@hotmail.com"
+            CreatedBy = email
         };
         var result = await handler.HandleAsync(requestWithUser, cancellationToken);
 
