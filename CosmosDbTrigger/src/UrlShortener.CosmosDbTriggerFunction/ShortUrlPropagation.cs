0b9bd73fb82e801d0bbc0d70000d3d7ccdd0bbe2
jeevan thapa <jeevanthapa@microsoft.com>
2025-01-11 13:11:42 -0500

    Edit fntrigger

--- CosmosDbTrigger/src/UrlShortener.CosmosDbTriggerFunction/ShortUrlPropagation.cs
+++ CosmosDbTrigger/src/UrlShortener.CosmosDbTriggerFunction/ShortUrlPropagation.cs
@@ -11,26 +11,25 @@ namespace UrlShortener.CosmosDbTriggerFunction
 
         [Function("ShortUrlPropagation")]
         public async Task Run([CosmosDBTrigger(
-                databaseName: "urls",
-                containerName: "items",
-                Connection = "CosmosDbConnection",
-                LeaseContainerName = "leases",
-                CreateLeaseContainerIfNotExists = true)]
-            IReadOnlyList<UrlDocument> input)
+            databaseName: "urls",
+            containerName: "items",
+            Connection = "CosmosDbConnection",
+            LeaseContainerName = "leases",
+            CreateLeaseContainerIfNotExists = true)] IReadOnlyList<UrlDocument>? input)
         {
-            if (input == null || input.Count <= 0) return;
-            
+            if (input is not { Count: > 0 }) return;
+
             foreach (var document in input)
             {
-                _logger.LogInformation("Short Url: {ShortUrl}", document.Id);
+                _logger.LogInformation("Short url: {ShortUrl}", document.Id);
                 try
                 {
                     var cosmosDbDocument = new ShortenedUrlEntity(
-                        document.LongUrl,
-                        document.Id,
-                        document.CreatedOn,
-                        document.CreatedBy
-                    );
+                        document.LongUrl, 
+                        document.Id, 
+                        document.CreatedOn, 
+                        document.CreatedBy);
+                    
                     await container.UpsertItemAsync(cosmosDbDocument, new PartitionKey(document.CreatedBy));
                 }
                 catch (Exception ex)
@@ -45,24 +44,24 @@ namespace UrlShortener.CosmosDbTriggerFunction
     public class UrlDocument
     {
         public string Id { get; set; }
+
         public DateTimeOffset CreatedOn { get; set; }
+
         public string CreatedBy { get; set; }
+
         public string LongUrl { get; set; }
     }
-    public class ShortenedUrlEntity(
-        string longUrl,
-        string shortUrl,
-        DateTimeOffset createdOn,
-        string createdBy)
+
+    public class ShortenedUrlEntity(string longUrl, string shortUrl, DateTimeOffset createdOn, string createdBy)
     {
         public string LongUrl { get; } = longUrl;
 
-        [JsonProperty(PropertyName = "id")] // Cosmos DB Unique Identifier
+        [JsonProperty(PropertyName = "id")] // Cosmos Db unique Identfier
         public string ShortUrl { get; } = shortUrl;
 
         public DateTimeOffset CreatedOn { get; } = createdOn;
 
-        [JsonProperty(PropertyName = "PartitionKey")] // Cosmos DB Partition Key
+        [JsonProperty(PropertyName = "PartitionKey")] // Cosmos Db partition key
         public string CreatedBy { get; } = createdBy;
     }
 }
